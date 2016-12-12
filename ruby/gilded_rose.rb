c6b6facf73009262b35d8a5d3eb00d7eccfd31f4
Michael Tobin <mikel.tobin@gmail.com>
2016-12-12 00:24:06 -0700

    Only 4 test failures left. Created a quality check function to ensure the quality is always 50 or less.

--- ruby/gilded_rose.rb
+++ ruby/gilded_rose.rb
@@ -12,53 +12,15 @@ class GildedRose
 
   def update_quality()
     @items.each do |item|
-      # if item.name != AGEDBRIE and item.name != BACKSTAGE_PASS
-      #   if item.quality > 0
-      #     if item.name != RAG
-      #       item.quality = item.quality - 1
-      #     end
-      #   end
-      # else
-      #   if item.quality < 50
-      #     item.quality = item.quality + 1
-      #     if item.name == BACKSTAGE_PASS
-      #       if item.sell_in < 11
-      #         if item.quality < 50
-      #           item.quality = item.quality + 1
-      #         end
-      #       end
-      #       if item.sell_in < 6
-      #         if item.quality < 50
-      #           item.quality = item.quality + 1
-      #         end
-      #       end
-      #     end
-      #   end
-      # end
-      # if item.name != RAG
-      #   item.sell_in = item.sell_in - 1
-      # end
-      # if item.sell_in < 0
-      #   if item.name == AGEDBRIE
-      #     increaseItemQuality(item)
-      #   elsif item.name == BACKSTAGE_PASS
-      #     decreaseItemQuality(item, item.quality)
-      #   elsif item.name == RAG
-      #   else
-      #     decreaseItemQuality(item, 1)
-      #   end
-      # end
-
-
       case item.name
         when RAG
           puts 'No Change to the Mighty Ragnaros Boon'
         when BACKSTAGE_PASS
           decreaseSellIn(item, -1)
-          updateItemQuality(item, 1)
+          updateBackstagePasses(item)
         when AGEDBRIE
           decreaseSellIn(item, -1)
-          updateItemQuality(item, 1)
+          updateAgedBrie(item)
         when CONJURED
           decreaseSellIn(item, -1)
           updateItemQuality(item, -2)
@@ -76,19 +38,50 @@ def decreaseSellIn(item, amount)
   end
 end
 
-def updateItemQuality(item, quality)
-  if item.quality > 0 && item.quality <= 50
+def updateBackstagePasses(item)
+  if item.quality > 0 && item.quality < 50
     if item.sell_in < 0
-      item.quality += (quality*2)
+      item.quality = 0
+    elsif item.sell_in > 10
+      item.quality += 1
+      checkQualityAmount(item)
+    elsif item.sell_in <= 10 && item.sell_in > 5
+      item.quality += 2
+      checkQualityAmount(item)
+    elsif item.sell_in <= 5
+      item.quality += 3
+      checkQualityAmount(item)
+    end
+  end
+end
+
+def checkQualityAmount(item)
+  if item.quality > 50
+    item.quality = 50
+  end
+end
+
+def updateItemQuality(item, quality)
+  if item.quality > 0 && item.quality < 50
+    if item.sell_in < 0
+      item.quality += -2
     else
       item.quality += quality
     end
   end
 end
 
-def increaseItemQuality(item, quality)
-  if item.quality >0 && item.quality < 50
-    item.quality += quality
+def updateAgedBrie(item)
+  if item.quality > 0 && item.quality < 50
+    if item.sell_in < 0
+      puts item.name
+      puts item.quality
+      item.quality += 2
+    else
+      puts item.name
+      puts item.quality
+      item.quality += 1
+    end
   end
 end
 
